22a4019af2818af0fdbc15958e8cf96263ffc0a9
Roger Pérez <90121735+rogerdanielzzz@users.noreply.github.com>
2023-02-15 23:43:26 -0300

    First

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Ejercicio 1
+Console.WriteLine("Intruduce tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Bienvenido " + nombre);
+//Ejercicio 2
+string hora = DateTime.Now.ToString("HH:mm:ss");
+
+Console.WriteLine("Ingresaste a las " + hora);
